3770151ed7b1065fbf67a470d6f7461a902247e5
Tatiana Chigorina <tanya199613@gmail.com>
2025-04-19 12:21:50 +0500

    +searchSelenideSoftAssertTest

--- src/test/java/GithubSearchTests.java
+++ src/test/java/GithubSearchTests.java
@@ -2,6 +2,10 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
 public class GithubSearchTests {
     @BeforeAll
     static void setUp(){
@@ -10,10 +14,20 @@ public class GithubSearchTests {
         Configuration.baseUrl = "https://github.com";
     }
     @Test
-    void fillFormSuccessTest(){
+    void searchSelenideSoftAssertTest(){
 //        Откройте страницу Selenide в Github
+        open("/");
+        $("button[placeholder = 'Search or jump to...']").click();
+        $("#query-builder-test").setValue("selenide").pressEnter();
+        $("[data-testid='results-list']").$$("div").first().$("a")
+                .shouldHave(attributeMatching("href","^.*\\/selenide\\/selenide$")).click();
 //        Перейдите в раздел Wiki проекта
+        $("#wiki-tab").click();
 //        Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        $("#wiki-body").$(byText("Chapters")).parent().sibling(0).$("li a[href*='Soft']")
+                .shouldHave(href("https://github.com/selenide/selenide/wiki/SoftAssertions")).click();
 //        Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+        $("#wiki-body").$("[id*='using-junit5']").parent().sibling(0)
+                .shouldHave(attributeMatching("class","^.*source-java.*$"));
     }
 }
